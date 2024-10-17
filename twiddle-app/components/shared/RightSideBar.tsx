d5c33756c35fe1e98627fa092d28757c21c428d5
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-10-17 19:39:36 +0300

    Creating the RightSidebar component and fetchUsers action

--- twiddle-app/components/shared/RightSideBar.tsx
+++ twiddle-app/components/shared/RightSideBar.tsx
@@ -1,6 +1,41 @@
-const RightSideBar = () => {
+import { fetchUsers } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs/server"
+
+const RightSideBar = async () => {
+    const user = await currentUser()
+    if(!user) return null
+
+    const similarMinds = await fetchUsers({
+        userId: user.id,
+        pageSize: 4
+    })
+
+
     return (
         <>
+            <section className="custom-scrollbar rightsidebar">
+                <div className="flex flex-1 flex-col justify-start">
+                    <h3 className="text-heading4-medium text-light-1">
+                        Suggestions
+                    </h3>
+                </div>
+                <div className="flex flex-col flex-1 justify-start">
+                    <h3 className="text-heading4-medium text-light-1">
+                        Groups
+                    </h3>
+                </div>
+
+                <div className="flex flex-col flex-1 justify-start">
+                    <h3 className="text-heading4-medium text-light-1">
+                        Users
+                    </h3>
+                    <div className="mt-7 flex w-[350px] flex-col gap-10">
+                        {
+                            similarMinds.users.length > 0 ?
+                        }
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- twiddle-app/lib/actions/user.actions.ts
+++ twiddle-app/lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface CreateUserParams {
     userId: String;
@@ -88,5 +89,50 @@ export const updateUser = async ({
 
     } catch(err: any) {
         throw new Error(`Failed to update user info: ${err.message}`)
+    }
+}
+
+export const fetchUsers = async ({
+    userId,
+    searchString = '',
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = 'desc'
+}: {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number
+    sortBy?: SortOrder
+}) => {
+    try {
+        connectToDB()
+        const skipAmount = (pageNumber - 1) * pageSize
+        const regex = new RegExp(searchString, 'i')
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }
+        }
+
+        if(searchString.trim() !== '') {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } }
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy }
+
+        const userQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize)
+
+        const totalUserCount = await User.countDocuments(query)
+        const users = await userQuery.exec()
+        const isNext = totalUserCount > skipAmount + users.length
+
+        return { users, isNext }
+    }  catch(err: any) {
+        throw new Error(`Failed to fetch users: ${err.message}`)
     }
 }
