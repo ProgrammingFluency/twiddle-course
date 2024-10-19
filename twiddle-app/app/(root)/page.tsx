0d8c5a83371c8878362c7c6751c425af1759f06b
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-10-20 01:44:12 +0300

    completing the Home page of twiddle

--- twiddle-app/app/(root)/page.tsx
+++ twiddle-app/app/(root)/page.tsx
@@ -1,5 +1,6 @@
+import TweetCard from '@/components/cards/TweetCard';
 import LandingPage from '@/components/shared/LandingPage'
-import { fetchTweets } from '@/lib/actions/tweet.actions';
+import { fetchTweets, isTweetByUser } from '@/lib/actions/tweet.actions';
 import { fetchUser } from '@/lib/actions/user.actions'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
@@ -26,12 +27,40 @@ export default async function Home({
       3
     );
 
-    
-
-
   return (
     <>
-      
+       <section className='mt-10 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No tweets found</p>
+        ) : (
+          <div>
+              {result.posts.map(async (tweet) => {
+                const isOwner = await isTweetByUser(userInfo?._id, tweet?._id)
+                return (
+                  <div className="mt-10">
+                    <TweetCard
+                      key={tweet._id}
+                      id={tweet._id}
+                      currentUserId={user.id}
+                      owner={isOwner}
+                      DB_userID={userInfo._id}
+                      retweetOf={tweet.retweetOf}
+                      parentId={tweet.parentId}
+                      content={tweet.text}
+                      author={tweet.author}
+                      group={tweet.group}
+                      createdAt={tweet.createdAt} 
+                      comments={tweet.children}
+                      likes={tweet.likes}
+                      liked={ userInfo.likedTweets.includes(tweet._id) }
+                    />
+                  </div>
+              )
+              }
+            )}
+          </div> 
+        )}
+      </section>
     </>
   )
 }
