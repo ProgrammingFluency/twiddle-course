a31cb24ec2266d7a2fdc1468d5237ec9a11fddd0
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-10-21 01:59:03 +0300

    Finishing the search feature

--- twiddle-app/app/(root)/search/page.tsx
+++ twiddle-app/app/(root)/search/page.tsx
@@ -1,3 +1,7 @@
+import GroupCard from "@/components/cards/GroupCard";
+import UserCard from "@/components/cards/UserCard";
+import Pagination from "@/components/shared/Pagination";
+import SearchBar from "@/components/shared/SearchBar";
 import { fetchGroups } from "@/lib/actions/group.actions";
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
@@ -26,6 +30,59 @@ const Page = async ({
         pageNumber: searchParams?.page ? +searchParams.page : 1,
         pageSize: 25
     })
+
+    return (
+        <>
+        <section>
+      <h1 className='head-text mb-10'>Search</h1>
+
+      <SearchBar routeType='search' />
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='text-light-1'>No User Results</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {groupsResult.groups.length === 0 ? (
+          <p className='text-light-1'>No Group Results</p>
+        ) : (
+          <>
+            {groupsResult.groups.map((group) => (
+              <GroupCard
+              key= {group.id}
+              id={group.id}
+              name={group.name}
+              username={group.username}
+              imgUrl={group.image}
+              members={group.members}
+          />
+            ))}
+          </>
+        )}
+      </div>
+
+      <Pagination
+        path='search'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </section>
+        </>
+    )
 
   }
 
