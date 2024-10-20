8c18b46cfd89a66abdf92cddcde6bd0d277a0769
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-10-20 21:12:52 +0300

    creating the fetchTweetById action

--- twiddle-app/app/(root)/tweet/[id]/page.tsx
+++ twiddle-app/app/(root)/tweet/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { fetchTweetById } from "@/lib/actions/tweet.actions"
 import { fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
--- twiddle-app/lib/actions/tweet.actions.ts
+++ twiddle-app/lib/actions/tweet.actions.ts
@@ -254,3 +254,52 @@ export const fetchTweets = async (pageNumber = 1, pageSize = 20) => {
     }
   
   };
+
+  export const fetchTweetById = async (id: string) => {
+    connectToDB();
+  
+    try {
+      const tweet = await Tweet.findById(id)
+        .populate({
+          path: 'author',
+          model: User,
+          select: '_id id name image',
+        })
+        .populate({
+          path: 'children',
+          populate: [
+            {
+              path: 'author',
+              model: User,
+              select: '_id id name image',
+            },
+            {
+              path: 'children',
+              model: Tweet,
+              populate: {
+                path: 'author',
+                model: User,
+                select: '_id id name image',
+              },
+            },
+          ],
+        })
+        .populate({
+          path: 'retweetOf', // Populate the retweetOf field
+          populate: {
+            path: 'author',
+            model: User,
+            select: '_id id name image',
+          },
+        })
+        .populate({
+          path: 'group',
+          model: Group,
+        })
+        .exec();
+  
+      return tweet;
+    } catch (err: any) {
+      throw new Error(`Error fetching tweet: ${err.message}`);
+    }
+  };
