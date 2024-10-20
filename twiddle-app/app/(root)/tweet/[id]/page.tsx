de28fe184b17fa002dfbb5c492056637106ecf16
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-10-20 21:07:07 +0300

    creating the tweet reply feature

---
+++ twiddle-app/app/(root)/tweet/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { fetchUser } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const Page = async ({params}: {params: {id: string}}) => {
+    const user = await currentUser()
+    if(!user) return null
+    
+    const userInfo = await fetchUser(user.id)
+    if(!userInfo?.onboarded) redirect('/onboarding')
+
+    const tweet = await fetchTweetById(params.id)
+
+}
+
+export default Page
